705598b443707a610377bffc439d6bdb7fbabffd
Augusto Bosco <augustobosco0@gmail.com>
2026-07-28 20:37:56 -0300

    Update +page.svelte

--- src/routes/media/+page.svelte
+++ src/routes/media/+page.svelte
@@ -5,6 +5,9 @@
 	import img2 from '$lib/assets/media/2.png';
 	import img3 from '$lib/assets/media/3.png';
 	import img4 from '$lib/assets/media/4.png';
+
+	let yt1 = $state(false);
+	let yt2 = $state(false);
 </script>
 
 <Navbar selected="media" />
@@ -23,27 +26,37 @@
 		  <source src="/MEDIA/HowlsMovingCastle/video-howl.mp4" type="video/mp4">
 		</video>
 
-		<iframe
-			class="video"
-			src="https://www.youtube.com/embed/jIgz_IhCmEY?si=eifrKg2jNKpt1p4P"
-			title="YouTube video player"
-			frameborder="0"
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-			referrerpolicy="strict-origin-when-cross-origin"
-			allowfullscreen
-			loading="lazy"
-		></iframe>
+		{#if yt1}
+			<iframe
+				class="video"
+				src="https://www.youtube.com/embed/jIgz_IhCmEY?autoplay=1"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				referrerpolicy="strict-origin-when-cross-origin"
+				allowfullscreen
+			></iframe>
+		{:else}
+			<button class="video fachada" style="background-image: url('https://img.youtube.com/vi/jIgz_IhCmEY/maxresdefault.jpg');" onclick={() => yt1 = true}>
+				<div class="boton-play">▶</div>
+			</button>
+		{/if}
 
-		<iframe 
-			class="video"
-			src="https://www.youtube.com/embed/nps4-HFdQBc?si=RQRn5OFdsZl98MZl" 
-			title="YouTube video player" 
-			frameborder="0" 
-			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-			referrerpolicy="strict-origin-when-cross-origin" 
-			allowfullscreen
-			loading="lazy"
-		></iframe>
+		{#if yt2}
+			<iframe 
+				class="video"
+				src="https://www.youtube.com/embed/nps4-HFdQBc?autoplay=1" 
+				title="YouTube video player" 
+				frameborder="0" 
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+				referrerpolicy="strict-origin-when-cross-origin" 
+				allowfullscreen
+			></iframe>
+		{:else}
+			<button class="video fachada" style="background-image: url('https://img.youtube.com/vi/nps4-HFdQBc/maxresdefault.jpg');" onclick={() => yt2 = true}>
+				<div class="boton-play">▶</div>
+			</button>
+		{/if}
 	</div>
 
 </div>
@@ -76,6 +89,37 @@
 		margin: 2rem auto;
 		width: 60%;
 		aspect-ratio: 16 / 9;
+	}
+
+	.fachada {
+		background-size: cover;
+		background-position: center;
+		cursor: pointer;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		background-color: black;
+		border: none;
+		padding: 0;
+	}
+
+	.boton-play {
+		font-size: 3rem;
+		color: white;
+		background-color: rgba(0, 0, 0, 0.7);
+		border-radius: 50%;
+		width: 80px;
+		height: 80px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding-left: 5px;
+		transition: background-color 0.2s, transform 0.2s;
+	}
+
+	.fachada:hover .boton-play {
+		background-color: red;
+		transform: scale(1.1);
 	}
 
 	@media (max-width: 1024px) {
