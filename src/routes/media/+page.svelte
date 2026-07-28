054336c987cf669a48d0258165eb8e5b2401511b
Augusto Bosco <augustobosco0@gmail.com>
2026-07-28 15:27:07 -0300

    Update +page.svelte

--- src/routes/media/+page.svelte
+++ src/routes/media/+page.svelte
@@ -18,7 +18,7 @@
 
 <iframe
 	class="video"
-	src="https://www.youtube.com/embed/LOQVj4U3RDA?si=NWlL2Ji5lN_PWeXY"
+	src="https://www.youtube.com/watch?v=_JwYA9JxTQA"
 	title="YouTube video player"
 	frameborder="0"
 	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
