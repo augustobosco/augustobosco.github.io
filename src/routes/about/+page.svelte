054d615f6803a23a22f49af9b6cb66404ad1460c
Augusto Bosco <augustobosco0@gmail.com>
2026-08-10 14:36:43 -0300

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -88,8 +88,8 @@
 			<div class="service-text">
 				<p class="service-title">Music Production & Additional Support</p>
 				<p class="service-content">
-					Additional music, editing, stem preparation and production support for composers and
-					creative teams, available on a per-cue, episode or complete-project basis.
+					Additional music, arrangement, editing, stem preparation and production support for 
+					composers and creative teams, available on a per-cue, episode or complete-project basis.
 				</p>
 			</div>
 		</div>
